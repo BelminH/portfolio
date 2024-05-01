97bfdc17fb3bff9a4ac670dbb4b3c2e2b75f0729
Belmin <belminhus@gmail.com>
2024-05-01 19:31:10 +0200

    init for page layout

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./app.scss"
+
+const App = () => {
+    return <div>
+        <section>Face</section>
+        <section>Parllax-Effect</section>
+        <section>Skills</section>
+        <section>Parllax-Effect</section>
+        <section>Personal-Projects1</section>
+        <section>Personal-Projects2</section>
+        <section>Contact</section>
+    </div>;
+};
+
+export default App;
